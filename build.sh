35d39f303c5dd91048a41e56b97b6860d227d6b8
ffais <ffais@fbk.eu>
2019-03-27 11:38:29 +0100

    fixed envfile bug

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ Msg="$TSSRV Build in corso"
 URL="https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
 CHAT="chat_id=${CHAT_ID}"
 curl -s -X POST $URL -d $CHAT -d "text=$Msg"
-ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$IP "sudo service innoweee-engine stop && /home/$USR/sources/deploy-innoweee-engine.sh && echo VER=${RELEASE} > /home/dev/innoweee-engine-env && sudo service innoweee-engine start "
+ssh -i sshkey -o "StrictHostKeyChecking no" $USR@$IP "sudo service innoweee-engine stop && /home/$USR/sources/deploy-innoweee-engine.sh && sed -i -E """s/^VER=[0-9.]+/VER=$RELEASE/g""" /home/dev/innoweee-engine-env && sudo service innoweee-engine start "
 statusCode=$?
 if [[ $statusCode -eq 0 ]]; then
   Msg="$TSSRV Aggiornamento completato"
