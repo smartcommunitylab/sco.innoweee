12d39e7f6b55952afbfb70162b52f61292ae2955
chin8 <matteo.chini@fbk.eu>
2019-04-17 15:40:48 +0200

    fixed enabled parts

--- webApp/src/app/members/recycle/item-classification/item-classification.page.ts
+++ webApp/src/app/members/recycle/item-classification/item-classification.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { ProfileService } from 'src/app/services/profile.service';
 import { MainPage } from 'src/app/class/MainPage';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Storage } from '@ionic/storage';
 import { NavController } from '@ionic/angular';
-
+import { APP_CONFIG_TOKEN, ApplicationConfig } from 'src/app/app-config';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 @Component({
   selector: 'app-item-classification',
   templateUrl: './item-classification.page.html',
@@ -15,11 +17,23 @@ import { NavController } from '@ionic/angular';
 export class ItemClassificationPage extends MainPage implements OnInit {
   item: any;
 
+  private itemSocketURL;
+  private apiEndpoint;
+  ws: any;
+  message: any;
+  disabled: boolean;
+  playerData: any;
+
   constructor(private route: ActivatedRoute,
     private profileService: ProfileService,
     public navCtrl: NavController, 
+    private router: Router,
+
+    @Inject(APP_CONFIG_TOKEN) private config: ApplicationConfig,
     public translate: TranslateService, public authService: AuthenticationService, public storage: Storage) {
     super(translate, authService, storage,navCtrl);
+    this.itemSocketURL = config.itemSocketURL;
+    this.apiEndpoint = config.apiEndpoint;
   }
 
   ngOnInit() {
@@ -32,7 +46,36 @@ export class ItemClassificationPage extends MainPage implements OnInit {
         console.log(this.item); // popular
       });
   }
+  ionViewWillEnter() {
 
+    this.profileService.getLocalPlayerData().then(res => {
+      this.playerData = res;
+      this.connect(this.playerData.tenantId, this.playerData.objectId);
+    })
+  }
+  connect(tenantId, playerId) {
+    //connect to stomp where stomp endpoint is exposed
+    let sock = new SockJS(this.apiEndpoint + this.itemSocketURL);
+    // let sock = new WebSocket("ws://localhost:2020/itemws/websocket");
+    this.ws = Stomp.over(sock);
+    let that = this;
+    this.ws.connect({}, function (frame) {
+      that.ws.subscribe("/errors", function (message) {
+        alert("Error " + message.body);
+      });
+      that.ws.subscribe("/topic/item." + tenantId + "." + playerId, function (message) {
+        console.log(message)
+        that.message = JSON.parse(message.body);
+        if (that.message && that.message.itemId) {
+          //go to item-loaded
+          that.router.navigate(['item-loaded', that.message.itemId, false]);
+        }
+      });
+      that.disabled = true;
+    }, function (error) {
+      console.log("STOMP error " + error);
+    });
+  }
   ionViewDidEnter() {
     super.ionViewDidEnter();
   }
