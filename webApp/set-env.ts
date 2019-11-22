0e6a8ebf3c506e36639c53becfc55aef7b57fdb0
Smart Community Lab <smartcommunitylab@smarts-mbp.fbkeduroam.it>
2019-11-22 17:15:41 +0100

    new  env

--- webApp/set-env.ts
+++ webApp/set-env.ts
@@ -1,6 +1,7 @@
-import { writeFile } from 'fs';
-import { argv } from 'yargs';
-
+// import { writeFile } from 'fs';
+// import { argv } from 'yargs';
+var fs = require("fs");
+var yargs = require("yargs");
 // This is good for local dev environments, when it's better to
 // store a projects environment variables in a .gitignore'd file
 require('dotenv').config();
@@ -8,7 +9,7 @@ require('dotenv').config();
 // Would be passed to script like this:
 // `ts-node set-env.ts --environment=dev`
 // we get it from yargs's argv object
-const environment = argv.environment;
+const environment = yargs.argv.environment;
 const isProd = environment === 'prod';
 
 const targetPath = `./src/environments/environment.${environment}.ts`;
@@ -42,10 +43,10 @@ export const environment = {
   superDoubleSecret: "${process.env.superDoubleSecret}" 
 };
 `
-writeFile(targetPath, envConfigFile, function (err) {
+
+fs.writeFile(targetPath, envConfigFile, function (err) {
   if (err) {
     console.log(err);
   }
-
   console.log(`Output generated at ${targetPath}`);
 });
