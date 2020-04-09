e0f70939f6b59a8d2aa48dbf69ab5db232ab6a2a
Matteo Chini <matteo.chini@gmail.com>
2020-04-09 08:58:13 +0200

    working on new ui (mobile app)

--- mobile/codeScanner/src/app/members/operatore/stats/stats.page.ts
+++ mobile/codeScanner/src/app/members/operatore/stats/stats.page.ts
@@ -26,6 +26,7 @@ export class StatsPage extends CommonPage implements OnInit {
     public dataServerService: DataServerService,
     public location: Location,
     public auth: AuthService,
+    private alertController: AlertController,
     private loadingController: LoadingController,
     public authService: AuthenticationService) {
     super(auth, router, translate, toastController, route, dataServerService, location, profileService, authService)
@@ -41,16 +42,47 @@ export class StatsPage extends CommonPage implements OnInit {
   async ngOnInit() {
     this.presentLoading();
     const token = await this.auth.getValidToken();
-    this.dataServerService.getOperatorStats(this.profileService.getDomainMemorized()["tenants"][0],this.profileService.getCollector(), token.accessToken).then(res => {
+    this.dataServerService.getOperatorStats(this.profileService.getDomainMemorized()["tenants"][0], this.profileService.getCollector(), token.accessToken).then(res => {
       console.log(JSON.stringify(res));
       if (res)
-      this.stat=res;
+        this.stat = res;
     })
   }
-home(){
-  this.navCtrl.navigateRoot('home-operator');
-}
+  home() {
+    this.navCtrl.navigateRoot('home-operator');
+  }
+  async legenda() {
+    const alert = await this.alertController.create({
+      header: "Il significato delle statistiche",
+      cssClass: 'stat-popup',
+      message: `
+      <div class="legenda-label">
+      <div class="legenda-label-title">Totale oggetti attesi:</div>
+      <div class="legenda-label-text">
+      corrisponde al numero di oggetti RICICLO che sono stati classificati dalle famiglie, confermati a scuola e conferiti negli smart bin, ma che devono ancora essere verificati dall’operatore. Il numero <b>diminuisce</b> man mano che gli oggetti attesi vengono verificati e confermati presso lo stabilimento.
+      </div>
+    </div>
+    <div class="legenda-label">
+      <div class="legenda-label-title">Totale oggetti corrispondenti:</div>
+      <div class="legenda-label-text">
+      corrisponde al numero di oggetti RICICLO che sono stati classificati dalle famiglie, confermati a scuola, conferiti negli smart bin, e che sono stati verificati dall’operatore. Il numero <b>aumenta</b> man mano che gli oggetti corrispondenti vengono verificati e confermati.
+      </div>
+    </div>
+    <div class="legenda-label">
+      <div class="legenda-label-title">Totale oggetti inattesi:</div>
+      <div class="legenda-label-text">
+      corrisponde al numero di oggetti che sono stati verificati e confermati dall’operatore presso lo stabilimento, ma che potrebbero non essere oggetti RICICLO, non avere l’etichetta, non essere stati classificati dalle famiglie, non essere stati confermati a scuola o non essere stati conferiti negli smart bin. Il numero aumenta man mano che l’operatore verifica e conferma gli oggetti che presentano le caratteristiche sopra riportate.      </div>
+    </div>`,
+      buttons: [
+         {
+          text: 'OK',
+          cssClass: 'secondary'
+        }
+      ]
+    });
 
+    await alert.present();
+  }
   getFooter() {
   }
 
